1026880c9cf4e1ec1a8b8522d45ae7e28d8d33f8
natmark <natmark0918@gmail.com>
2017-04-03 09:05:39 +0900

    :new: Add project

---
+++ GitHubRepositoryViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubRepositoryViewer
+//
+//  Created by AtsuyaSato on 2017/04/03.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
